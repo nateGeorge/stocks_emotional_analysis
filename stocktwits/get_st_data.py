5e58a4a009b5bccca8efe68282d7f17405a9e671
nateGeorge <wordsforthewise@gmail.com>
2018-07-22 12:20:25 -0600

    add stocktwits api

---
+++ stocktwits/get_st_data.py
@@ -0,0 +1,115 @@
+import os
+import time
+import pickle as pk
+
+import api
+
+
+def make_dirs(path):
+    """
+    makes directory, one folder at a time
+    """
+    last_dir = cu.get_home_dir()
+    new_path = path
+    if last_dir in path:
+        new_path = path.replace(last_dir, '')
+
+    for d in new_path.split('/'):
+        last_dir = os.path.join(last_dir, d)
+        if not os.path.exists(last_dir):
+            os.mkdir(last_dir)
+
+
+def get_home_dir(repo_name='stocks_emotional_analysis'):
+    cwd = os.getcwd()
+    cwd_list = cwd.split('/')
+    repo_position = [i for i, s in enumerate(cwd_list) if s == repo_name]
+    if len(repo_position) > 1:
+        print("error!  more than one intance of repo name in path")
+        return None
+
+    home_dir = '/'.join(cwd_list[:repo_position[0] + 1]) + '/'
+    return home_dir
+
+
+def scrape_historical_data(ticker='AAPL', verbose=True):
+    filepath = get_home_dir() + 'stocktwits/data/' + ticker + '/'
+    if not os.path.exists(filepath): make_dirs(filepath)
+    filename = filepath + ticker + '_all_messages.pk'
+    if os.path.exists(filename):
+        with open(filename, 'rb') as f:
+            all_messages = pk.load(f)
+        earliest = min([m['id'] for m in all_messages])  #should also be able to do this: all_messages[-1]['id']
+    else:
+        print('no previously existing file')
+        earliest = None
+        all_messages = []
+
+    start = time.time()
+    # returns a dictionary with keys ['cursor', 'response', 'messages', 'symbol']
+    # docs say returns less than or equal to max, but seems to be only less than
+    st, req_left, reset_time = api.get_stock_stream('AAPL', {'max': earliest})
+    all_messages.extend(st['messages'])
+
+    num_calls = 1
+
+    while True:
+        if st['cursor']['more']:
+            if verbose:
+                print('getting more data, made', str(num_calls), 'calls so far')
+                print(str(req_left), 'requests left')
+            time_elapsed = time.time() - start
+            if time_elapsed < 3600 and num_calls > 350 or req_left < 10:
+                print('made too many calls too fast, need to change access token or')
+                print('wait another', str(round((3600 - time_elapsed) / 60)), 'minutes')
+                print('made', str(num_calls), 'calls in', str(round(time_elapsed)), 'seconds')
+
+                # overwrite old data with updated data
+                with open(filename, "wb") as f:
+                    pk.dump(all_messages, f)
+
+                # TODO: sleep until reset time from header is met
+                time.sleep(3601 - time_elapsed)
+                start = time.time()
+                num_calls = 0
+
+            earliest = st['cursor']['max']
+            st, req_left, reset_time = api.get_stock_stream('AAPL', {'max': earliest})
+            all_messages.extend(st['messages'])
+            num_calls += 1
+            # seems to take long enough that we don't need to sleep
+            # time.sleep(0.117)
+        else:
+            print('reached end of data')
+            break
+
+    # write data with updates
+    with open(filename, "wb") as f:
+        pk.dump(all_messages, f)
+
+#
+# import pandas as pd
+# df = pd.io.json.json_normalize(all_messages)
+# df['created_at'] = pd.to_datetime(df['created_at'])
+# # drop empty columns
+# df.drop(['entities.sentiment', 'reshare_message.message.entities.sentiment'], axis=1, inplace=True)
+# # is the bearish/bullish tag
+# df['entities.sentiment.basic'].value_counts()
+#
+# # most useful columns
+# useful_df = df[['body', 'created_at', 'entities.sentiment.basic', 'likes.total', 'user.followers']]
+#
+#
+# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+#
+# analyzer = SentimentIntensityAnalyzer()
+#
+# def get_sentiments_vader(x):
+#     vs = analyzer.polarity_scores(x)
+#     return pd.Series([vs['compound'], vs['pos'], vs['neg'], vs['neu']], index=['compound', 'pos', 'neg', 'neu'])
+#
+# sentiments = useful_df['body'].apply(get_sentiments_vader)
+# sentiments.hist()
+
+
+# meld with price data and see if any correlations
---
+++ stocktwits/requestors.py
@@ -0,0 +1,118 @@
+""" A variety of classes using different libraries to implement `get_json` and `post_json` methods.
+"""
+import os
+import json
+import logging as log
+# Try to import modules needed for Google App Engine just in case
+try:
+    from google.appengine.api import urlfetch
+    from google.appengine.runtime import DeadlineExceededError
+    import urllib
+    import json
+except:
+    pass
+
+# Try to import requests just in case
+try:
+    import requests
+except:
+    pass
+
+
+# StockTwits details
+ST_BASE_URL = 'https://api.stocktwits.com/api/2/'
+at_name = 'stock_twits_testing_at'  # name of access token variable in ~/.bashrc
+ST_BASE_PARAMS = dict(access_token=os.getenv(at_name))
+
+__author__ = 'Jason Haury'
+
+
+def get_header_info(headers):
+    """
+    gets crucial header info such as how many requests left and when it resets
+    """
+    return int(headers['X-RateLimit-Remaining']), int(headers['X-RateLimit-Reset'])
+
+
+class Requests():
+    """ Uses `requests` library to GET and POST to Stocktwits, and also to convert resonses to JSON
+    """
+    def get_json(url, params=None):
+        """ Uses tries to GET a few times before giving up if a timeout.  returns JSON
+        """
+        resp = None
+        for i in range(4):
+            try:
+                resp = requests.get(url, params=params, timeout=5)
+                # to get headers:
+                # resp.headers
+                # contains how many requests are left and how long till reset
+            except requests.Timeout:
+                trimmed_params = {k: v for k, v in params.items() if k not in ST_BASE_PARAMS.keys()}
+                log.error('GET Timeout to {} w/ {}'.format(url[len(ST_BASE_URL):], trimmed_params))
+            if resp is not None:
+                break
+        if resp is None:
+            log.error('GET loop Timeout')
+            return None, None
+        else:
+            header_info = get_header_info(resp.headers)
+            return json.loads(resp.content.decode('utf-8')), header_info[0], header_info[1]
+
+    def post_json(url, params=None, deadline=30):
+        """ Tries to post a couple times in a loop before giving up if a timeout.
+        """
+        resp = None
+        for i in range(4):
+            try:
+                resp = requests.post(url, params=params, timeout=5)
+            except requests.Timeout:
+                trimmed_params = {k: v for k, v in params.items() if k not in ST_BASE_PARAMS.keys()}
+                log.error('POST Timeout to {} w/ {}'.format(url[len(ST_BASE_URL):], trimmed_params))
+            if resp is not None:
+                break
+        # TODO wrap in appropriate try/except
+        header_info = get_header_info(resp.headers)
+        return json.loads(resp.content.decode('utf-8')), header_info[0], header_info[1]
+
+
+
+class GAE():
+    """ A wrapper around Google App Engine's `urlfetch` to make it act like `requests` package
+    """
+    def get_json(url, params=None):
+        """ Uses tries to GET a few times before giving up if a timeout.  returns JSON
+        """
+        params = ('?' + urllib.urlencode(params)) if params else ''  # URL query string parameters (Access Token, etc)
+        resp = None
+        for i in range(4):
+            try:
+                resp = urlfetch.fetch(url+params, method='GET')
+            except:
+                trimmed_params = {k: v for k, v in params.items() if k not in ST_BASE_PARAMS.keys()}
+                log.error('GET Timeout to {} w/ {}'.format(url[len(ST_BASE_URL):], trimmed_params))
+            if resp is not None:
+                break
+        if resp is None:
+            log.error('GET loop Timeout')
+            return None, None
+        else:
+            header_info = get_header_info(resp.headers)
+            return json.loads(resp.content.decode('utf-8')), header_info[0], header_info[1]
+
+    def post_json(url, params=None, deadline=30):
+        """ Tries to post a couple times in a loop before giving up if a timeout.
+        """
+        params = '?' + urllib.urlencode(params) if params else ''  # URL query string parameters (Access Token)
+        resp = None
+        for i in range(4):
+            try:
+                resp = urlfetch.fetch(url+params, method='POST', deadline=deadline)
+            except DeadlineExceededError:
+                trimmed_params = {k: v for k, v in params.items() if k not in ST_BASE_PARAMS.keys()}
+                log.error('POST Timeout to {} w/ {}'.format(url[len(ST_BASE_URL):], trimmed_params))
+            if resp is not None:
+                break
+        # TODO wrap in appropriate try/except
+        header_info = get_header_info(resp.headers)
+        return json.loads(resp.content.decode('utf-8')), header_info[0], header_info[1]
