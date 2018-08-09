47f00160d126da94254c67b279e8d578fea7b50d
nateGeorge <wordsforthewise@gmail.com>
2018-08-08 19:24:04 -0600

    add ability to update with latest data

--- stocktwits/get_st_data.py
+++ stocktwits/get_st_data.py
@@ -3,6 +3,7 @@ import time
 import pickle as pk
 
 import pandas as pd
+import numpy as np
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 import api
@@ -35,22 +36,76 @@ def get_home_dir(repo_name='stocks_emotional_analysis'):
     return home_dir
 
 
-def scrape_historical_data(ticker='AAPL', verbose=True):
+def check_new_data(latest, st, all_messages, all_new_messages, filename, new_filename):
+    new_ids = [m['id'] for m in st['messages']]
+    if latest in new_ids:
+        print('got all new data')
+        new_msg_idx = np.where(latest == np.array(new_ids))[0][0]
+        new_messages = st['messages'][:new_msg_idx]
+        all_new_messages.extend(new_messages)
+        all_messages = all_new_messages + all_messages
+        write_files(all_messages, filename, all_new_messages, new_filename, only_update_latest=True)
+        # delete temp new_messages file
+        if os.path.exists(new_filename):
+            os.remove(new_filename)
+
+        return True
+
+    return False
+
+
+def write_files(all_messages, filename, new_messages=None, new_filename=None, only_update_latest=False):
+    if only_update_latest:
+        # check to make sure not None
+        if new_messages is None:
+            print('new messages is None, not saving anything')
+            return
+        elif new_filename is None:
+            print('new_filename is None, not saving anything')
+            return
+
+        with open(new_filename, "wb") as f:
+            pk.dump(new_messages, f, -1)
+        with open(filename, "wb") as f:
+            pk.dump(all_messages, f, -1)
+    else:
+        with open(filename, "wb") as f:
+            pk.dump(all_messages, f, -1)
+
+
+def scrape_historical_data(ticker='AAPL', verbose=True, only_update_latest=False):
+    """
+    only_update_latest=True will fill in any data from present back to latest existing data
+    """
+    #TODO: deal with misssing data in the middle somehow...
     filepath = get_home_dir() + 'stocktwits/data/' + ticker + '/'
     if not os.path.exists(filepath): make_dirs(filepath)
     filename = filepath + ticker + '_all_messages.pk'
+    new_filename = filepath + ticker + '_new_messages.pk'  # for new data when updating
     if os.path.exists(filename):
         with open(filename, 'rb') as f:
             all_messages = pk.load(f)
-        earliest = min([m['id'] for m in all_messages])  #should also be able to do this: all_messages[-1]['id']
+
+        ids = [m['id'] for m in all_messages]
+        earliest = min(ids)  #should also be able to do this: all_messages[-1]['id']
+        latest = max(ids)
     else:
         print('no previously existing file')
         earliest = None
+        latest = None
         all_messages = []
+
+    if only_update_latest:
+        # too hard to deal with existing new messages, just start over if partially completed
+        earliest = None
+        new_messages = []
+    else:
+        new_messages = None
 
     start = time.time()
     # returns a dictionary with keys ['cursor', 'response', 'messages', 'symbol']
     # docs say returns less than or equal to max, but seems to be only less than
+    # get first request
     st, req_left, reset_time = api.get_stock_stream(ticker, {'max': earliest})
     if st is None:
         print('returned None for some reason')
@@ -58,7 +113,13 @@ def scrape_historical_data(ticker='AAPL', verbose=True):
         print('tried all access tokens, none have enough calls left')
         return
     else:
-        all_messages.extend(st['messages'])
+        if only_update_latest:
+            new_messages.extend(st['messages'])
+            # see if we got all the new data yet
+            if check_new_data(latest, st, all_messages, new_messages, filename, new_filename):
+                return
+        else:
+            all_messages.extend(st['messages'])
 
     num_calls = 1
 
@@ -67,8 +128,7 @@ def scrape_historical_data(ticker='AAPL', verbose=True):
             # save every 50 calls just in case something happens
             if num_calls % 50 == 0:
                 # overwrite old data with updated data
-                with open(filename, "wb") as f:
-                    pk.dump(all_messages, f)
+                write_files(all_messages, filename, new_messages, new_filename, only_update_latest)
             if verbose:
                 print('getting more data, made', str(num_calls), 'calls so far')
                 print(str(req_left), 'requests left')
@@ -79,8 +139,7 @@ def scrape_historical_data(ticker='AAPL', verbose=True):
                 print('made', str(num_calls), 'calls in', str(round(time_elapsed)), 'seconds')
 
                 # overwrite old data with updated data
-                with open(filename, "wb") as f:
-                    pk.dump(all_messages, f)
+                write_files(all_messages, filename, new_messages, new_filename, only_update_latest)
 
                 # TODO: sleep until reset time from header is met
                 time.sleep(3601 - time_elapsed)
@@ -92,15 +151,22 @@ def scrape_historical_data(ticker='AAPL', verbose=True):
             if st is None:
                 print('returned None for some reason')
                 # overwrite old data with updated data
-                with open(filename, "wb") as f:
-                    pk.dump(all_messages, f)
+                write_files(all_messages, filename, new_messages, new_filename, only_update_latest)
                 st = {'cursor': {'more': True}}
                 continue
             elif st is False:
                 print('tried all access tokens, stopping for now')
                 break
 
-            all_messages.extend(st['messages'])
+            if only_update_latest:
+                if check_new_data(latest, st, all_messages, new_messages, filename, new_filename):
+                    return
+                    break  # just in case
+
+                new_messages.extend(st['messages'])
+            else:
+                all_messages.extend(st['messages'])
+
             num_calls += 1
             # seems to take long enough that we don't need to sleep
             # time.sleep(0.117)
@@ -109,11 +175,10 @@ def scrape_historical_data(ticker='AAPL', verbose=True):
             break
 
     # write data with updates
-    with open(filename, "wb") as f:
-        pk.dump(all_messages, f)
+    write_files(all_messages, filename, new_messages, new_filename, only_update_latest)
 
 
-def get_sentiments_vader(x):
+def get_sentiments_vader(analyzer, x):
     vs = analyzer.polarity_scores(x)
     return pd.Series([vs['compound'], vs['pos'], vs['neg'], vs['neu']], index=['compound', 'pos', 'neg', 'neu'])
 
@@ -129,7 +194,9 @@ def load_historical_data(ticker='AAPL'):
         return None
 
     df = pd.io.json.json_normalize(all_messages)
+    # convert datetime column to datetime and set as index
     df['created_at'] = pd.to_datetime(df['created_at'])
+    tsla_st.set_index('created_at', inplace=True)
     # drop empty columns
     df.drop(['entities.sentiment', 'reshare_message.message.entities.sentiment'], axis=1, inplace=True)
     # is the bearish/bullish tag
@@ -139,19 +206,37 @@ def load_historical_data(ticker='AAPL'):
     useful_df = df[['body', 'created_at', 'entities.sentiment.basic', 'likes.total', 'user.followers']]
 
     analyzer = SentimentIntensityAnalyzer()
-
-    sentiments = useful_df['body'].apply(get_sentiments_vader)
+    """
+    https://github.com/cjhutto/vaderSentiment#about-the-scoring
+    compound: most useful single metric -- average valence of each word in sentence
+    """
+    sentiments = useful_df['body'].apply(lambda x: get_sentiments_vader(analyzer, x))
     # sentiments.hist()
     full_useful = pd.concat([useful_df, sentiments], axis=1)
+
+    # convert entities.sentiment.basic to +1 for bullish and -1 for bearish, 0 for NA
+    conv_dict = {'Bearish': -1, np.nan: 0, 'Bullish': 1}
+    tsla_st['entities.sentiment.basic'] = tsla_st['entities.sentiment.basic'].replace(conv_dict)
 
     return full_useful
 
 
 # meld with price data and see if any correlations
-import sys
-sys.path.append('../../scrape_ib')
-import scrape_ib
+# import sys
+# sys.path.append('../../scrape_ib')
+# import scrape_ib
+# ticker = 'TSLA'
+# tsla_3min = scrape_ib.load_data(ticker)
+# tsla_st = load_historical_data(ticker)
+# tsla_st = tsla_st.iloc[::-1]  # reverse order from oldest -> newest to match IB data
+# tsla_st_min = tsla_st[['entities.sentiment.basic', 'compound', 'pos', 'neg', 'neu']]
+# #TWS bars are labeled by the start of the time bin, so  use label='left'
+# tsla_st_min_3min = tsla_st_min.resample('3min', label='left').mean()
+
+# TODO: take everything after market close and before open and make a new feature out of it??
+
+# resample tsla_st to 3_min data to match 3min bars
+
 ticker = 'TSLA'
-tsla_3min = scrape_ib.load_data(ticker)
-tsla_st = load_historical_data(ticker)
-tsla_st = tsla_st.iloc[::-1]
+scrape_historical_data(ticker, only_update_latest=True)
+# scrape_historical_data(ticker, only_update_latest=False)
