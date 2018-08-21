f1fd0a769d230775b08e9cf8762f86c79754606b
nateGeorge <wordsforthewise@gmail.com>
2018-08-20 20:55:09 -0600

    sleep for 5 mins instead of exiting function; put loading into functoin

--- stocktwits/get_st_data.py
+++ stocktwits/get_st_data.py
@@ -9,8 +9,17 @@ import pandas as pd
 import numpy as np
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import dask.dataframe as dd
+# from swifter import swiftapply
 
 import api  # local file in same directory
+
+# import plotting libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import sys
+sys.path.append('../../scrape_ib')
+import scrape_ib
 
 
 def make_dirs(path):
@@ -87,6 +96,7 @@ def scrape_historical_data(ticker='AAPL', verbose=True, only_update_latest=False
     filename = filepath + ticker + '_all_messages.pk'
     new_filename = filepath + ticker + '_new_messages.pk'  # for new data when updating
     if os.path.exists(filename):
+        print("existing file")
         with open(filename, 'rb') as f:
             all_messages = pk.load(f)
 
@@ -103,7 +113,9 @@ def scrape_historical_data(ticker='AAPL', verbose=True, only_update_latest=False
         # too hard to deal with existing new messages, just start over if partially completed
         earliest = None
         new_messages = []
+        print('going to update only the latest messages')
     else:
+        print('going to get the earliest messages to the end')
         new_messages = None
 
     start = time.time()
@@ -159,8 +171,8 @@ def scrape_historical_data(ticker='AAPL', verbose=True, only_update_latest=False
                 st = {'cursor': {'more': True}}
                 continue
             elif st is False:
-                print('tried all access tokens, stopping for now')
-                break
+                print('tried all access tokens, sleeping for 5 mins')
+                time.sleep(5*60)
 
             if only_update_latest:
                 if check_new_data(latest, st, all_messages, new_messages, filename, new_filename):
@@ -258,96 +270,181 @@ def get_eastern_market_open_close():
     return open_days
 
 
-# meld with price data and see if any correlations
-import sys
-sys.path.append('../../scrape_ib')
-import scrape_ib
-
-ticker = 'TSLA'
-tsla_3min = scrape_ib.load_data(ticker)
-tsla_st = load_historical_data(ticker)
-tsla_st = tsla_st.iloc[::-1]  # reverse order from oldest -> newest to match IB data
-tsla_st_min = tsla_st[['entities.sentiment.basic', 'compound', 'pos', 'neg', 'neu']]
-#TWS bars are labeled by the start of the time bin, so  use label='left'
-tsla_st_min_3min = tsla_st_min.resample('3min', label='left').mean().ffill()  # forward fill because it's not frequent enough
-
-
-# go through each unique date, get market open and close times from IB data,
-# then get average of sentiment features from market closed times and add as new feature
-unique_dates = np.unique(tsla_st_min_3min.index.date)
-open_days = get_eastern_market_open_close()
-last_close = None
-# new_feats = {}
-new_feats = pd.DataFrame()
-for d in unique_dates:
-    if d in open_days.index:  # if it's a trading day...
-        open_time = open_days.loc[d]['market_open']
-        if last_close is None:
-            new_feats[d] = tsla_st_min_3min.loc[:open_time].mean()
-        else:
-            new_feats[d] = tsla_st_min_3min.loc[last_close:open_time].mean()
-
-        last_close = open_days.loc[d]['market_open']
-
-new_feats = new_feats.T
-new_feats.columns = ['bearish_bullish_closed', 'compound_closed', 'pos_closed', 'neg_closed', 'neu_closed']
-
-tsla_st_full_3min = tsla_st_min_3min.copy()
-nf_cols = new_feats.columns
-for d in new_feats.index:
-    for c in nf_cols:
-        tsla_st_full_3min.loc[d:d + pd.Timedelta('1D'), c] = new_feats.loc[d, c]
-
-# merge sentiment with price data
-full_tsla = pd.concat([tsla_3min, tsla_st_full_3min], axis=1)
-full_tsla = full_tsla.loc[tsla_3min.index]
-
-# TODO: get post volume as a feature...looks like high post volume increase, high sentiment, and high short % is a good combo for incr in price (MTCH)
-
-# make 1h price change and 1h future price change
-full_tsla['close_1h_chg'] = full_tsla['close'].pct_change(20)  # for 3 min, 20 segments is 60 mins
-# full_tsla['3m_future_price'] = full_tsla['close'].shift(-1)  # sanity check
-full_tsla['1h_future_price'] = full_tsla['close'].shift(-20)
-full_tsla['1h_future_price_chg'] = full_tsla['1h_future_price'].pct_change(20)
-
-full_tsla['8h_future_price'] = full_tsla['close'].shift(-20*8)
-full_tsla['8h_future_price_chg'] = full_tsla['1h_future_price'].pct_change(20*8)
-
-# import plotting libraries
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# plt.scatter(full_tsla['bearish_bullish_closed'], full_tsla['1h_future_price_chg'])
+def combine_with_price_data(ticker='TSLA'):
+    # meld with price data and see if any correlations
+    trades_3min = scrape_ib.load_data(ticker)  # by default loads 3 mins bars
+    st = load_historical_data(ticker)  # get stock twits data
+    st = st.iloc[::-1]  # reverse order from oldest -> newest to match IB data
+    # create counts column for resampling
+    st['count'] = 1
+    st_min = st[['entities.sentiment.basic', 'compound', 'pos', 'neg', 'neu', 'count']]
+    #TWS bars are labeled by the start of the time bin, so  use label='left'
+    st_min_3min = st_min.resample('3min', label='left').mean().ffill()  # forward fill because it's not frequent enough
+    st_min_3min['count'] = st['count'].resample('3min', label='left').sum().ffill()
 
 
-f = plt.figure(figsize=(12, 12))
-sns.heatmap(full_tsla.corr(), annot=True)
-plt.tight_layout()
+    # go through each unique date, get market open and close times from IB data,
+    # then get average of sentiment features from market closed times and add as new feature
+    unique_dates = np.unique(st_min_3min.index.date)
+    open_days = get_eastern_market_open_close()
+    last_close = None
+    # new_feats = {}
+    new_feats = pd.DataFrame()
+    for d in unique_dates:
+        if d in open_days.index:  # if it's a trading day...
+            open_time = open_days.loc[d]['market_open']
+            if last_close is None:
+                new_feats[d] = st_min_3min.loc[:open_time].mean()
+                new_feats[d]['count'] = st_min_3min.loc[:open_time, 'count'].sum()
+            else:
+                new_feats[d] = st_min_3min.loc[last_close:open_time].mean()
+                new_feats[d]['count'] = st_min_3min.loc[last_close:open_time, 'count'].sum()
 
-plt.scatter(full_tsla['bearish_bullish_closed'], full_tsla['8h_future_price_chg'])
-plt.scatter(full_tsla['entities.sentiment.basic'], full_tsla['8h_future_price_chg'])
+            last_close = open_days.loc[d]['market_open']
 
-plt.scatter(full_tsla['opt_vol_high'], full_tsla['8h_future_price_chg'])
+    new_feats = new_feats.T
+    new_feats.columns = ['bearish_bullish_closed', 'compound_closed', 'pos_closed', 'neg_closed', 'neu_closed', 'count_closed']
 
-# when it was -0.05, strong correlation to negative returns -- all on one day, 3/28
-plt.scatter(full_tsla['compound_closed'], full_tsla['8h_future_price_chg'])
+    st_full_3min = st_min_3min.copy()
+    nf_cols = new_feats.columns
+    for d in new_feats.index:
+        for c in nf_cols:
+            st_full_3min.loc[d:d + pd.Timedelta('1D'), c] = new_feats.loc[d, c]
+
+    # merge sentiment with price data
+    full_df = pd.concat([trades_3min, st_full_3min], axis=1)
+    full_df = full_df.loc[trades_3min.index]
+
+    # resample to daily frequency
+    st_daily = st_min.resample('1D', label='left').mean().ffill()
+    st_daily['count'] = st_min['count'].resample('1D', label='left').sum().ffill()
+    st_daily_std = st_min.resample('1D', label='left').std().ffill()
+
+    # count column is all 0s for some reason
+    st_daily_std.drop('count', inplace=True, axis=1)
+    st_daily_std.columns = [c + '_std' for c in tsla_st_daily_std.columns]
+
+    # tsla_st_daily_full = pd.concat([tsla_st_daily, tsla_st_daily_std], axis=1)
+    trades_1d = trades_3min.resample('1D').apply({'open': 'first',
+                                            'high': 'max',
+                                            'low': 'min',
+                                            'close': 'last',
+                                            'volume': 'sum',
+                                            'bid_open': 'first',
+                                            'bid_high': 'max',
+                                            'bid_low': 'min',
+                                            'bid_close': 'last',
+                                            'ask_open': 'first',
+                                            'ask_high': 'max',
+                                            'ask_low': 'min',
+                                            'ask_close': 'last',
+                                            'opt_vol_open': 'first',
+                                            'opt_vol_high': 'max',
+                                            'opt_vol_low': 'min',
+                                            'opt_vol_close': 'last'})
+
+    # make new feature for sentiment action over weekends and closed days
+    # monday = 0, sunday = 6
+    # TODO:
+    weekend_feats = pd.DataFrame()
+    temp_feats = pd.DataFrame()
+    for i in st_daily.index:
+        if i not in trades_1d.index:
+            temp_feats
 
 
-# get daily price change and daily bearish/bullish and sentiments
+    full_daily = pd.concat([trades_1d, st_daily, st_daily_std], axis=1)
+    for i in range(1, 11):
+        full_daily[str(i) + 'd_future_price'] = full_daily['close'].shift(-i)
+        full_daily[str(i) + 'd_future_price_chg_pct'] = full_daily[str(i) + 'd_future_price'].pct_change(i)
+        # drop future price column because we care about % change
+        full_daily.drop(str(i) + 'd_future_price', axis=1, inplace=True)
 
-tsla_1d = tsla_3min.resample('1D').apply({'open': 'first',
-                                        'high': 'max',
-                                        'low': 'min',
-                                        'close': 'last'})
 
-# resample tsla_st to 3_min data to match 3min bars
+
+    f = plt.figure(figsize=(20, 20))
+    sns.heatmap(full_daily.corr(), annot=True)
+    plt.tight_layout()
+
+    # slight positive correlation
+    plt.scatter(full_daily['entities.sentiment.basic_std'], full_daily['10d_future_price_chg_pct'])
+
+    plt.scatter(full_daily['entities.sentiment.basic'], full_daily['10d_future_price_chg_pct'])
+
+    plt.scatter(full_daily['compound'], full_daily['10d_future_price_chg_pct'])
+    plt.scatter(full_daily['pos'], full_daily['10d_future_price_chg_pct'])
+
+    # get things correlated with 5d change > 0.1
+    idx_5d = np.where(full_daily.columns == '5d_future_price_chg_pct')[0][0]
+    idx_1d = np.where(full_daily.columns == '1d_future_price_chg_pct')[0][0]
+    full_daily.columns[:idx_1d][(full_daily.corr() > 0.1).iloc[:idx_1d, idx_5d]]
+
+    # TODO: get post volume as a feature...looks like high post volume increase, high sentiment, and high short % is a good combo for incr in price (MTCH)
+
+    # make 1h price change and 1h future price change
+    full_df['close_1h_chg'] = full_df['close'].pct_change(20)  # for 3 min, 20 segments is 60 mins
+    # full_tsla['3m_future_price'] = full_tsla['close'].shift(-1)  # sanity check
+    full_df['1h_future_price'] = full_df['close'].shift(-20)
+    full_df['1h_future_price_chg'] = full_df['1h_future_price'].pct_change(20)
+
+    full_df['8h_future_price'] = full_df['close'].shift(-20*8)
+    full_df['8h_future_price_chg'] = full_df['1h_future_price'].pct_change(20*8)
+
+
+def plot_combined_data(full_df):
+    # plt.scatter(full_tsla['bearish_bullish_closed'], full_tsla['1h_future_price_chg'])
+    f = plt.figure(figsize=(12, 12))
+    sns.heatmap(full_df.corr(), annot=True)
+    plt.tight_layout()
+
+    plt.scatter(full_df['bearish_bullish_closed'], full_df['8h_future_price_chg'])
+    plt.scatter(full_df['entities.sentiment.basic'], full_df['8h_future_price_chg'])
+
+    plt.scatter(full_df['opt_vol_high'], full_df['8h_future_price_chg'])
+
+    # when it was -0.05, strong correlation to negative returns -- all on one day, 3/28
+    plt.scatter(full_df['compound_closed'], full_df['8h_future_price_chg'])
+
+
+    # get daily price change and daily bearish/bullish and sentiments
+
+    # don't think I nede this anymore
+    # tsla_1d = tsla_3min.resample('1D').apply({'open': 'first',
+    #                                         'high': 'max',
+    #                                         'low': 'min',
+    #                                         'close': 'last'})
+
+# resample bearish/bullish to daily, get number of counts of posts
 
 # ticker = 'TSLA'
 # scrape_historical_data(ticker, only_update_latest=True)
 # scrape_historical_data(ticker, only_update_latest=False)
 
-tickers = ['AMD', 'IQ', 'LNG', 'MU', 'SNAP', 'MTCH']
-for t in tickers:
-    print('scraping', t)
-    scrape_historical_data(t)
-    # time.sleep(60 * 10)
+# reached end of data
+['LNG', 'IQ', ]
+
+# no messages
+['XLM']
+
+# tickers = ['AMD', 'IQ', 'MU', 'SNAP', 'MTCH', 'BTC', 'ETH', 'ETH.X'
+#             'SPY', 'BABA', 'TVIX', 'OSTK', 'MU', 'QQQ', 'SNAP', 'TTD', 'VKTX',
+#             'OMER', 'OLED']
+#
+# tickers =  tickers + ['ALNY', 'OMER', 'FOLD', 'RDFN', 'TUR', 'TXMD', 'TDOC', 'SQ',
+#             'PYPL', 'ADBE', 'FB', 'BOX', 'Z', 'TGT', 'FMC', 'KIRK', 'FTD',
+#             'ABEV', 'GE', 'F', 'TTT', 'DDD', 'VSAT', 'TKC', 'NWSA']
+# while True:
+#     for t in tickers:
+#         print('scraping', t)
+#         scrape_historical_data(t)
+#         time.sleep(60 * 10)
+
+
+# scrape_historical_data('TSLA', only_update_latest=True)
+#
+# # find large gap in data for missing intermediate data
+# tsla = load_historical_data('TSLA')
+# index_diff = tsla.index[:-1] - tsla.index[1:]
+# max_diff_idx = np.argmax(index_diff)
+# tsla.iloc[max_diff_idx]
+# tsla.iloc[max_diff_idx + 1]
